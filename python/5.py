0e2296a931024b475a8538b25d27238784c30a3f
Erdajt Sopjani <120386306+ErdajtSopjani@users.noreply.github.com>
2023-12-02 01:52:06 +0100

    Update 5.py

--- python/5.py
+++ python/5.py
@@ -1,7 +1,7 @@
 def is_palindrome(x):
-   number_string = str(x)
-   reversed_string = number_string[::-1]
-   return number_string == reversed_string
+    number_string = str(x)
+    reversed_string = number_string[::-1]
+    return number_string == reversed_string
 
 print("\n\n\n" + str(is_palindrome(121)))
 print(is_palindrome(212))
