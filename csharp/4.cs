c43c78721d2068f63365f522d6551987038fc8e9
ErdajtSopjani <erdajt.sopjani@bgt.school>
2024-01-15 11:52:25 +0100

    fixed c# snippets spacing

--- csharp/4.cs
+++ csharp/4.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 
 public class Solution {
-  static void Main(string[] args) {
-    ListNode head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
-    Console.WriteLine(IsPalindrome(head));
-
-    Console.Read();
-  }
 
   public static bool IsPalindrome(ListNode head) {
     StringBuilder sb = new StringBuilder();
